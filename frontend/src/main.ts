b6325a7d98169256daeb46d41d1c686c452ebab9
pakel <marija-nikolic@outlook.com>
2024-05-22 14:02:52 +0200

    provide routes added

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,14 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { FormsModule } from '@angular/forms';
-import { NgxEditorModule } from 'ngx-editor';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import {importProvidersFrom} from '@angular/core';
+import {AppComponent} from './app/app.component';
+import {FormsModule} from '@angular/forms';
+import {NgxEditorModule} from 'ngx-editor';
+import {BrowserModule, bootstrapApplication} from '@angular/platform-browser';
+import {provideRouter} from "@angular/router";
+import {appRoutes} from "./app/app-routes";
 
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, NgxEditorModule, FormsModule)]
+  providers: [importProvidersFrom(BrowserModule, NgxEditorModule, FormsModule),
+    provideRouter(appRoutes)]
 })
   .catch(err => console.error(err));
