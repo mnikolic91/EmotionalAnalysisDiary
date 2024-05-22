c294f6b86daa7b3039ad486ab40f34c163829fef
pakel <marija-nikolic@outlook.com>
2024-05-22 14:02:37 +0200

    rename

---
+++ frontend/src/app/app-routes.ts
@@ -0,0 +1,11 @@
+import {Routes} from '@angular/router';
+import {MainComponent} from "./components/main/main.component";
+
+export const appRoutes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {
+    path: 'home',
+    component: MainComponent
+  }
+];
+
