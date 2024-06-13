c2478157a20e7324b8ce121e228e2aad59432792
pakel <marija-nikolic@outlook.com>
2024-06-13 19:29:34 +0200

    input components update

--- frontend/src/app/components/input/input.component.ts
+++ frontend/src/app/components/input/input.component.ts
@@ -1,17 +1,22 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import { Editor, NgxEditorModule } from "ngx-editor";
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {Editor, NgxEditorModule} from "ngx-editor";
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {ApiService} from "../../services/api.service";
+import {UserInput} from "../../models/user-input.model";
 
 @Component({
-    selector: 'app-input',
-    templateUrl: './input.component.html',
-    styleUrls: ['./input.component.css'],
-    standalone: true,
-    imports: [NgxEditorModule, ReactiveFormsModule, FormsModule]
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+  standalone: true,
+  imports: [NgxEditorModule, ReactiveFormsModule, FormsModule]
 })
 export class InputComponent implements OnInit, OnDestroy {
   editor!: Editor;
   html = '';
+  userInputText!: string;
+
+  apiService = inject(ApiService)
 
   ngOnInit(): void {
     this.editor = new Editor();
@@ -19,5 +24,13 @@ export class InputComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.editor.destroy();
+  }
+
+
+  onSubmit() {
+    const newUserInput: UserInput = {text: this.userInputText, date: new Date()};
+    this.apiService.createUserInput(newUserInput).subscribe(response => {
+      console.log('User input created:', response);
+    });
   }
 }
