dc7f96539e9d638e1ca31d9cc278e16f66468779
pakel <marija-nikolic@outlook.com>
2024-06-13 19:28:53 +0200

    routes update

--- frontend/src/app/app-routes.ts
+++ frontend/src/app/app-routes.ts
@@ -1,11 +1,16 @@
 import {Routes} from '@angular/router';
 import {MainComponent} from "./components/main/main.component";
+import {HomeComponent} from "./components/home/home.component";
 
 export const appRoutes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {
-    path: 'home',
+    path: 'input',
     component: MainComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
 
