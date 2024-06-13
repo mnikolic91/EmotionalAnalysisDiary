7ca923e0098cca7422fb0fceac823d476d66ec36
pakel <marija-nikolic@outlook.com>
2024-06-13 19:29:44 +0200

    main update

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,14 @@
-import {importProvidersFrom} from '@angular/core';
-import {AppComponent} from './app/app.component';
-import {FormsModule} from '@angular/forms';
-import {NgxEditorModule} from 'ngx-editor';
-import {BrowserModule, bootstrapApplication} from '@angular/platform-browser';
-import {provideRouter} from "@angular/router";
-import {appRoutes} from "./app/app-routes";
-
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { FormsModule } from '@angular/forms';
+import { NgxEditorModule } from 'ngx-editor';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from "@angular/router";
+import { appRoutes } from "./app/app-routes";
+import { HttpClientModule } from "@angular/common/http";
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, NgxEditorModule, FormsModule),
+  providers: [importProvidersFrom(BrowserModule, NgxEditorModule, FormsModule, HttpClientModule),
     provideRouter(appRoutes)]
 })
   .catch(err => console.error(err));
