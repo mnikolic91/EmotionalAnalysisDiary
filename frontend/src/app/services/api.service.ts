cacbb36cd41a493a36275fbdbbf4ad19863ffbd3
pakel <marija-nikolic@outlook.com>
2024-06-14 13:47:11 +0200

    service update

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -10,28 +10,28 @@ import {AverageMonthScores} from '../models/average-month-scores.model';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:8000/api';
+  private apiUrl = '/api';
 
   http= inject(HttpClient)
 
 
   getUserInputs(): Observable<UserInput[]> {
-    return this.http.get<UserInput[]>(`${this.apiUrl}/user-inputs`);
+    return this.http.get<UserInput[]>(`${this.apiUrl}/user-input/`);
   }
 
   createUserInput(userInput: UserInput): Observable<UserInput> {
-    return this.http.post<UserInput>(`${this.apiUrl}/user-inputs`, userInput);
+    return this.http.post<UserInput>(`${this.apiUrl}/user-input/`, userInput);
   }
 
   getSentimentEmotions(): Observable<SentimentEmotion[]> {
-    return this.http.get<SentimentEmotion[]>(`${this.apiUrl}/sentiment-emotions`);
+    return this.http.get<SentimentEmotion[]>(`${this.apiUrl}/sentimentemotion/`);
   }
 
   getAverageWeekScores(): Observable<AverageWeekScores[]> {
-    return this.http.get<AverageWeekScores[]>(`${this.apiUrl}/average-week-scores`);
+    return this.http.get<AverageWeekScores[]>(`${this.apiUrl}/averageweek/`);
   }
 
   getAverageMonthScores(): Observable<AverageMonthScores[]> {
-    return this.http.get<AverageMonthScores[]>(`${this.apiUrl}/average-month-scores`);
+    return this.http.get<AverageMonthScores[]>(`${this.apiUrl}/averagemonth/`);
   }
 }
