7eec40d9e0fb8a3f19c523d22884ffdd48c0addc
pakel <marija-nikolic@outlook.com>
2024-06-20 14:10:10 +0200

    about us component added

--- frontend/src/app/app-routes.ts
+++ frontend/src/app/app-routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {MainComponent} from "./components/main/main.component";
 import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
 
 export const appRoutes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -11,6 +12,10 @@ export const appRoutes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+    {
+    path: 'about',
+    component: AboutComponent
   }
 ];
 
