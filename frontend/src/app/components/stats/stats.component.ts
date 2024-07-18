5151d3df4bc61eeeac572d818f199eec7b2b5237
pakel <marija-nikolic@outlook.com>
2024-07-18 13:01:00 +0200

    temp charts added, looks ugly will fix

--- frontend/src/app/components/stats/stats.component.ts
+++ frontend/src/app/components/stats/stats.component.ts
@@ -9,13 +9,14 @@ import { UserInput } from '../../models/user-input.model';
 import { TruncateWordsPipe } from "../../truncate-words.pipe";
 import { AverageWeekScores } from '../../models/average-week-scores.model';
 import { AverageMonthScores } from '../../models/average-month-scores.model';
+import {NgxChartsModule} from "@swimlane/ngx-charts";
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.css'],
   imports: [
-    RouterLink, NgbPagination, AsyncPipe, DatePipe, ReactiveFormsModule, TruncateWordsPipe
+    RouterLink, NgbPagination, AsyncPipe, DatePipe, ReactiveFormsModule, TruncateWordsPipe, NgxChartsModule
   ],
   standalone: true
 })
@@ -31,10 +32,24 @@ export class StatsComponent implements OnInit, OnDestroy {
   searchInput = new FormControl('');
   page = 1;
 
+  // Variables for ngx-charts
+  view: [number, number] = [700, 400];
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Date';
+  showYAxisLabel = true;
+  yAxisLabel = 'Score';
+  animations = true;
+  chartData: any[] = [];
+
   ngOnInit(): void {
     this.getUserInputs({page: this.page});
     this.getAverageWeekScores({page: this.page});
     this.getAverageMonthScores({page: this.page});
+    this.updateChartData('inputs'); // Initialize chart with default data
   }
 
   ngOnDestroy(): void {
@@ -65,6 +80,32 @@ export class StatsComponent implements OnInit, OnDestroy {
 
   showData(dataType: string): void {
     this.currentData = dataType;
+    this.updateChartData(dataType); // Update chart data on button click
+  }
+
+  updateChartData(dataType: string): void {
+    if (dataType === 'inputs') {
+      this.inputs$.subscribe(data => {
+        this.chartData = data.map(input => ({
+          name: input.date,
+          value: input.text.length // Or some other metric you want to visualize
+        }));
+      });
+    } else if (dataType === 'averageWeekScores') {
+      this.averageWeekScores$.subscribe(data => {
+        this.chartData = data.map(score => ({
+          name: score.week,
+          value: score.avg_sentiment_score
+        }));
+      });
+    } else if (dataType === 'averageMonthScores') {
+      this.averageMonthScores$.subscribe(data => {
+        this.chartData = data.map(score => ({
+          name: score.month,
+          value: score.avg_sentiment_score
+        }));
+      });
+    }
   }
 
 }
